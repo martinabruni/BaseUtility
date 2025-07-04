7b4b48405e10d5f7a97b2e0b5663d10d667a0d1b
Martina Bruni <martina.bruni@agic.it>
2025-07-04 23:19:04 +0200

    feat(response-message): update version and enhance messages
    Update project version to 2.1.5. Add constructor to ResponseMessageOption
    class with optional entityName parameter. Modify message properties to
    use entityName for more dynamic and context-specific responses.

--- Options/ResponseMessageOption.cs
+++ Options/ResponseMessageOption.cs
@@ -1,18 +1,18 @@
 namespace BaseUtility
 {
-    public class ResponseMessageOption
+    public class ResponseMessageOption(string? entityName = "Entity")
     {
         public string InvalidRequest { get; set; } = "Invalid request.";
-        public string EntityCreatedSuccessfully { get; set; } = "Entity created successfully.";
-        public string ErrorCreatingEntity { get; set; } = "Error creating entity";
-        public string EntityNotFound { get; set; } = "Entity not found.";
-        public string EntityDeletedSuccessfully { get; set; } = "Entity deleted successfully.";
-        public string ErrorDeletingEntity { get; set; } = "Error deleting entity";
+        public string EntityCreatedSuccessfully { get; set; } = $"{entityName} created successfully.";
+        public string ErrorCreatingEntity { get; set; } = $"Error creating {entityName}";
+        public string EntityNotFound { get; set; } = $"{entityName} not found.";
+        public string EntityDeletedSuccessfully { get; set; } = $"{entityName} deleted successfully.";
+        public string ErrorDeletingEntity { get; set; } = $"Error deleting {entityName}";
         public string EntitiesRetrievedSuccessfully { get; set; } = "Entities retrieved successfully.";
         public string ErrorRetrievingEntities { get; set; } = "Error retrieving entities";
-        public string EntityRetrievedSuccessfully { get; set; } = "Entity retrieved successfully.";
-        public string ErrorRetrievingEntity { get; set; } = "Error retrieving entity";
-        public string EntityUpdatedSuccessfully { get; set; } = "Entity updated successfully.";
-        public string ErrorUpdatingEntity { get; set; } = "Error updating entity";
+        public string EntityRetrievedSuccessfully { get; set; } = $"{entityName} retrieved successfully.";
+        public string ErrorRetrievingEntity { get; set; } = $"Error retrieving {entityName}";
+        public string EntityUpdatedSuccessfully { get; set; } = $"{entityName} updated successfully.";
+        public string ErrorUpdatingEntity { get; set; } = $"Error updating {entityName}";
     }
 }
