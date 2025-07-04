6497bc62aa81b1f98629b4051c65e06a561e1231
Martina Bruni <martina.bruni@agic.it>
2025-07-05 00:40:00 +0200

    chore(release): update version to 2.1.8
    Update project version in `BaseUtility.csproj` to 2.1.8.
    Refactor `ResponseMessageOption` class by removing the constructor
    and replacing dynamic entity names with static string values for
    response messages. Add new properties `NotLoggedIn` and
    `ErrorMapping` for improved feedback.

--- Options/ResponseMessageOption.cs
+++ Options/ResponseMessageOption.cs
@@ -1,20 +1,22 @@
 namespace BaseUtility
 {
-    public class ResponseMessageOption(string? entityName = "Entity")
+    public class ResponseMessageOption
     {
         public string InvalidRequest { get; set; } = "Invalid request.";
-        public string EntityCreatedSuccessfully { get; set; } = $"{entityName} created successfully.";
-        public string ErrorCreatingEntity { get; set; } = $"Error creating {entityName}";
-        public string EntityNotFound { get; set; } = $"{entityName} not found.";
-        public string EntityDeletedSuccessfully { get; set; } = $"{entityName} deleted successfully.";
-        public string ErrorDeletingEntity { get; set; } = $"Error deleting {entityName}";
+        public string EntityCreatedSuccessfully { get; set; } = "Entity created successfully.";
+        public string ErrorCreatingEntity { get; set; } = "Error creating Entity";
+        public string EntityNotFound { get; set; } = "Entity not found.";
+        public string EntityDeletedSuccessfully { get; set; } = "Entity deleted successfully.";
+        public string ErrorDeletingEntity { get; set; } = "Error deleting Entity";
         public string EntitiesRetrievedSuccessfully { get; set; } = "Entities retrieved successfully.";
         public string ErrorRetrievingEntities { get; set; } = "Error retrieving entities";
-        public string EntityRetrievedSuccessfully { get; set; } = $"{entityName} retrieved successfully.";
-        public string ErrorRetrievingEntity { get; set; } = $"Error retrieving {entityName}";
-        public string EntityUpdatedSuccessfully { get; set; } = $"{entityName} updated successfully.";
-        public string ErrorUpdatingEntity { get; set; } = $"Error updating {entityName}";
+        public string EntityRetrievedSuccessfully { get; set; } = "Entity retrieved successfully.";
+        public string ErrorRetrievingEntity { get; set; } = "Error retrieving Entity";
+        public string EntityUpdatedSuccessfully { get; set; } = "Entity updated successfully.";
+        public string ErrorUpdatingEntity { get; set; } = "Error updating Entity";
         public string InvalidCredentials { get; set; } = "Invalid credentials provided.";
         public string NotImplemented { get; set; } = "This feature is not implemented yet.";
+        public string NotLoggedIn { get; set; } = "Login to perform this action.";
+        public string ErrorMapping { get; set; } = "Error mapping data.";
     }
 }
